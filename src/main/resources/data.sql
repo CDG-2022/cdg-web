807849278390b83dc94e0d64297d7342100f28e1
kanghoon-choi <kanghoon.choi@tosspayments.com>
2022-09-25 22:17:24 +0900

    #1 feat: 초기화 스크립트 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO notice (user_id, title, contents) VALUES (1, '제목1', '내용');
+INSERT INTO notice (user_id, title, contents) VALUES (1, '제목2', '내용');
+INSERT INTO notice (user_id, title, contents) VALUES (1, '제목3', '내용');
+INSERT INTO notice (user_id, title, contents) VALUES (1, '제목4', '내용');
+INSERT INTO notice (user_id, title, contents) VALUES (1, '제목5', '내용');
